fabc73431c826bc5124a1ffda515b8ac3f7619df
rikotacards <max.hsu@bgl.hk>
2019-10-31 20:44:25 +0800

    Change button styling.

--- src/VenuePage/ContactContainer.tsx
+++ src/VenuePage/ContactContainer.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Grid, Button, Typography, Theme, makeStyles } from "@material-ui/core";
 import { SidePanelHeader } from "./SidePanelHeader";
-import { EmailForm } from './EmailForm'
+import { EmailForm } from "./EmailForm";
 
 interface ContactContainerProps {
   phone: number | string;
   email: number | string;
 }
 
-
-
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
-     padding: theme.spacing(1),
-  }, 
-
-}))
+    padding: theme.spacing(1)
+  },
+  button:{
+    width: '100%',
+    padding: theme.spacing(1),
+  }
+}));
 
 export const ContactContainer: React.FunctionComponent<
   ContactContainerProps
@@ -25,34 +26,44 @@ export const ContactContainer: React.FunctionComponent<
   const [showNumber, toggleShowNumber] = React.useState(false);
   const [showEmail, toggleShowEmail] = React.useState(false);
   const contactCard = () => {
-    if (showNumber){
-      return (
-        <Typography>{phone}</Typography>
-      )
+    if (showNumber) {
+      return <Typography>{phone}</Typography>;
     }
-    if (showEmail){
-      return (
-        <EmailForm /> 
-      )
+    if (showEmail) {
+      return <EmailForm />;
     }
-    return null
-  }
-
+    return null;
+  };
 
   return (
     <>
-    <SidePanelHeader headerText={'Contact Venue'}/>
-    <Grid container={true} classes={classes}>
-      <Grid item={true} xs={6}>
-        <Button onClick={() => toggleShowNumber(!showNumber)} variant='contained'>Call</Button>
+      <SidePanelHeader headerText={"Contact Venue"} />
+      <Grid container={true} classes={classes} spacing={1}>
+        <Grid item={true} xs={6}>
+          <Button
+            onClick={() => toggleShowNumber(!showNumber)}
+            variant="contained"
+            color="primary"
+            className={classes.button}
+
+          >
+            Call
+          </Button>
+        </Grid>
+        <Grid item={true} xs={6}>
+          <Button
+            onClick={() => toggleShowEmail(!showEmail)}
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Email
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          {contactCard()}
+        </Grid>
       </Grid>
-      <Grid item={true} xs={6}>
-        <Button onClick= {() => toggleShowEmail(!showEmail)} variant='contained'>Email</Button>
-      </Grid>
-      <Grid item xs={12}>
-        {contactCard()}
-      </Grid>
-    </Grid>
     </>
   );
 };

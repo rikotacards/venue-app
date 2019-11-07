a1b6cdae3d2715dc4a9ff0d4fcf14acea401faa3
rikotacards <max.hsu@bgl.hk>
2019-11-07 23:15:36 +0800

    Add buttonlink and listlink.

---
+++ src/AppBar/ButtonLink.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Omit } from "@material-ui/types";
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps
+} from "react-router-dom";
+import {
+  Button
+} from "@material-ui/core";
+
+interface ButtonLinkProps {
+  icon?: React.ReactElement;
+  primary: string;
+  to: string;
+  className?: Record<"root", string>;
+}
+
+export const ButtonLink: React.FunctionComponent<ButtonLinkProps> = props => {
+  const { icon, primary, to, className } = props;
+
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef<
+        HTMLAnchorElement,
+        Omit<RouterLinkProps, "innerRef" | "to">
+      >((itemProps, ref) => (
+        // With react-router-dom@^6.0.0 use `ref` instead of `innerRef`
+        // See https://github.com/ReactTraining/react-router/issues/6056
+        <RouterLink to={to} {...itemProps} innerRef={ref} />
+      )),
+    [to]
+  );
+
+  return <Button component={renderLink} className={className && className.root} >{primary}</Button>;
+};

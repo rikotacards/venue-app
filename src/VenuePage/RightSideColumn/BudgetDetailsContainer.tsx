b8305a1925d3551e1bb64f9e20720734f684ba91
rikotacards <max.hsu@bgl.hk>
2019-12-08 13:39:01 +0800

    Add pricing disclaimer.

--- src/VenuePage/RightSideColumn/BudgetDetailsContainer.tsx
+++ src/VenuePage/RightSideColumn/BudgetDetailsContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, makeStyles, Theme, Typography } from "@material-ui/core";
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import { SidePanelHeader } from './SidePanelHeader'
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import { SidePanelHeader } from "./SidePanelHeader";
 interface BudgetDetailsContainerProps {
   perHeadBudget: number | string;
 }
@@ -9,28 +9,55 @@ interface BudgetDetailsContainerProps {
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     padding: theme.spacing(1),
-    textAlign: 'left'
+    textAlign: "left",
+    display: "flex"
   },
-    item: {
-      display: 'flex', 
-      flexDirection: 'row'
-    }
+  item: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  DetailsContainer: {
+    flexDirection: "column",
+    display: "flex",
+    textAlign: "left"
+  },
+  iconContainer: {
+    paddingRight: theme.spacing(2)
+  },
+  disclaimerContaier: {
+    textAlign: "left",
+    padding: theme.spacing(1),
+    lineHeight: 0,
+  },
+  fontSize: {
+    fontSize: 'xx-small'
+  }
 }));
 
-export const BudgetDetailsContainer: React.FunctionComponent<
-  BudgetDetailsContainerProps
-> = props => {
+export const BudgetDetailsContainer: React.FunctionComponent<BudgetDetailsContainerProps> = props => {
   const { perHeadBudget } = props;
   const classes = useStyles();
 
   return (
     <>
       <SidePanelHeader headerText={"pricing"} />
-      <Grid container={true} classes={classes}>
-        <Grid item={true} xs={12} className={classes.item}>
-        <AttachMoneyIcon/> <Typography>{perHeadBudget}</Typography>
-        </Grid>
-      </Grid>
+      <div className={classes.root}>
+        <div className={classes.iconContainer}>
+          <AttachMoneyIcon />
+        </div>
+        <div>
+          <Typography variant="subtitle2">Price per head</Typography>
+
+          <Typography variant="caption">{perHeadBudget} HKD</Typography>
+        </div>
+      </div>
+      <hr />
+      <div className={classes.disclaimerContaier}>
+        <Typography className={classes.fontSize} variant="caption">
+          *prices above are guide prices only supplied by the venue and may
+          fluctuate depending on client requirements
+        </Typography>
+      </div>
     </>
   );
 };
